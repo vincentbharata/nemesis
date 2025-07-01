97c4d9377f56dc2b106974b7d1c58eecbdc63459
vincentbharata <vincentbharataa@gmail.com>
2025-07-01 17:26:43 +0700

    challenge answer operator-conditional

--- exercise/day 1/conditional/challenge-5.kt
+++ exercise/day 1/conditional/challenge-5.kt
@@ -1,5 +1,11 @@
-fun main(){
-    val umur = readLine()?.toIntOrNull()
-    val vip = readLine()?.toBoolean() ?: false
-    val status 
+fun main() {
+    print("Umur: ")
+    val umur = readLine()?.toIntOrNull() ?: 0
+    print("Member VIP (true/false): ")
+    val isVip = readLine()?.toBoolean() ?: false
+    if (umur > 18 && isVip) {
+        println("Status: Diperbolehkan masuk")
+    } else {
+        println("Status: Tidak diperbolehkan masuk")
+    }
 }
